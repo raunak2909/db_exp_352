01af7126931d9ab51ffd9d86deba6603c5a65b17
raunakpurohit <raunak.wscubetech@gmail.com>
2025-02-17 20:13:34 +0530

    added bottomsheet to get notes from user

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,6 +7,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
   DbHelper? mDb;
 
   List<Map<String, dynamic>> mData = [];
@@ -46,12 +48,69 @@ class _HomePageState extends State<HomePage> {
           );
       }) : Center( child: Text("No notes yet!!"),),
       floatingActionButton: FloatingActionButton(onPressed: () async{
-        bool check =  await mDb!.addNote(title: "NewNote", desc: "Live life king size!!");
 
-        if(check){
-          print("Note Added");
-          getAllNotes();
-        }
+        titleController.clear();
+        descController.clear();
+
+        showModalBottomSheet(context: context, builder: (_){
+          return Container(
+            padding: EdgeInsets.all(11),
+            color: Colors.white,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Text("Add Note", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),),
+                SizedBox(
+                  height: 11,
+                ),
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    label: Text("Title"),
+                    hintText: "Enter title here..",
+                    focusedBorder: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                TextField(
+                  controller: descController,
+                  decoration: InputDecoration(
+                    label: Text("Desc"),
+                    hintText: "Enter desc here..",
+                    focusedBorder: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(onPressed: () async{
+                      bool check = await mDb!.addNote(title: titleController.text, desc: descController.text);
+
+                      if(check){
+                        getAllNotes();
+                        Navigator.pop(context);
+                      }
+
+                    }, child: Text('Add')),
+                    SizedBox(
+                      width: 11,
+                    ),
+                    OutlinedButton(onPressed: (){
+
+                    }, child: Text("Cancel")),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
 
       }, child: Icon(Icons.add),),
     );
